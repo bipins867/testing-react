33aad7da32718c0d4330eee8b8bf4f52ae251020
= <=>
2024-01-07 11:02:17 +0530

    Implementing the Link Tag

--- pages/abouts/index.js
+++ pages/abouts/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
+const details = [
+  { id: 1, name: "Yash", role: "Senior Developer" },
+  { id: 2, name: "Vaibhav", role: "Backend Developer" },
+  { id: 3, name: "Suresh", role: "Frontend Developer" },
+];
 export default () => {
+  const liList = details.map((item) => {
+    const id = item.id;
+    return (
+      <li key={item.id}>
+        <Link href={`/abouts/${id}`}>{item.name}</Link>
+      </li>
+    );
+  });
   return (
     <>
       <h1>This is all about the web page</h1>
+      <ul>{liList}</ul>
     </>
   );
 };
